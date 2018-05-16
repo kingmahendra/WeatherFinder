23ae4f87616a275aef4afe94d6a9880bcf095bc9
Mahendra Kumar <mahendrakumar@mahendras-mbp.home>
2018-05-16 17:10:50 +0200

    moving api key to config file

---
+++ src/appconfig.js
@@ -0,0 +1,2 @@
+ const API_KEY= 'd6d091ab1a32a6e22f178d8cbc1f5401'
+ export default API_KEY;
--- src/services/AppService.js
+++ src/services/AppService.js
@@ -1,11 +1,11 @@
 
+import API_KEY from '../appconfig'
 
 export const convertToCelcius= (temperatureInKelvin) => {
     return Math.round(temperatureInKelvin - 273.15);
 }
 
 export const apiRequest = (city, country) => {
-    const API_KEY= 'd6d091ab1a32a6e22f178d8cbc1f5401'
     const url = `http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&APPID=${API_KEY}&unit=metric`
     return fetch(url).then(data => data.json())
 }
